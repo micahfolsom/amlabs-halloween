747afc7ed168f6fe4d62d1e80a96eb6afb93d657
Micah Folsom <micahfolsom@gmail.com>
2024-06-19 22:28:31 -0700

    Add werewolf godot project

---
+++ running-ww/running_scene.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var left_pressed = Input.is_action_just_pressed("ui_left")
+	if (left_pressed):
+		print('left_pressed: ', left_pressed)
+	var right_pressed = Input.is_action_just_pressed("ui_right")
+	if (right_pressed):
+		print('right_pressed: ', right_pressed)
