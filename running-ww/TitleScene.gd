e7ebb9578508a0dc6eda40ce8612aa58f66736bf
Micah Folsom <micahfolsom@gmail.com>
2024-06-24 22:16:23 -0700

    Start adding new screens, countdown timer, etc

---
+++ running-ww/TitleScene.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var run_scene = preload("res://running_scene.tscn").instantiate()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var up_pressed = Input.is_action_just_pressed("ui_up")
+	if (up_pressed):
+		print('up_pressed: ', up_pressed)
+	var right_pressed = Input.is_action_just_pressed("ui_right")
+	if (right_pressed):
+		print('right_pressed: ', right_pressed)
+		get_tree().change_scene_to_file("res://running_scene.tscn")
