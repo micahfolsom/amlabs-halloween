5992b08d0eecf33f24078451e0dc0a2aa74aa0f2
ocdev-mg <ocdev.mindless.games@gmail.com>
2024-07-02 16:20:00 -0400

    fixed the bug, using is_action_just_pressed and window_Get_mode

--- running-ww/game_manager.gd
+++ running-ww/game_manager.gd
@@ -1,18 +1,16 @@
 extends Node
 
-var isFullscreen = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("Toggle_Fullscreen"):
-		isFullscreen = not isFullscreen
+	if Input.is_action_just_pressed("Toggle_Fullscreen"):
+		var windowMode = DisplayServer.window_get_mode()
 		
-		if isFullscreen:
+		if windowMode == 0:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
