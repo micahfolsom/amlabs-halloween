c9ecb63f293463117e80480c5719e499a56c35b3
Micah Folsom <micahfolsom@gmail.com>
2024-08-27 19:29:54 -0700

    Remove jump to score screen from title

--- running-ww/TitleScene.gd
+++ running-ww/TitleScene.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-#var run_scene = preload("res://running_scene.tscn").instantiate()
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var up_pressed = Input.is_action_just_pressed("ui_up")
-	if (up_pressed):
-		print('up_pressed: ', up_pressed)
-		get_tree().change_scene_to_file("res://score_scene.tscn")
+	pass
+	
+func _input(event):
 	var right_pressed = Input.is_action_just_pressed("ui_right")
 	if (right_pressed):
 		print('right_pressed: ', right_pressed)
