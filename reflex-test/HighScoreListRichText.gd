83ced5b632d86a6dde04782f21f2ae6bf67cd511
Micah Folsom <micahfolsom@gmail.com>
2025-08-24 13:09:23 -0700

    a little cleanup

--- reflex-test/HighScoreListRichText.gd
+++ reflex-test/HighScoreListRichText.gd
@@ -1,15 +1,8 @@
 extends RichTextLabel
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	install_effect(RichTextPulse.new())
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _process_custom_fx(char_fx : CharFXTransform) -> bool:
 	var color = char_fx.env.get("color1", char_fx.color)
