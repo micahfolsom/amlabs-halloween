e3e92feb972cf78c14c78282031416c8542482f0
Micah Folsom <micahfolsom@gmail.com>
2025-09-04 19:27:43 -0700

    add coin sound

--- reflex-test/title_scene.gd
+++ reflex-test/title_scene.gd
@@ -5,3 +5,7 @@ func _input(_event):
 	var accept_pressed = Input.is_action_just_pressed("ui_accept")
 	if (accept_pressed):
 		GameManager.change_to_scene_path("res://reflex_game.tscn")
+		
+	var coin_pressed = Input.is_action_just_pressed("coin_button")
+	if coin_pressed:
+		$CoinSound.play()
